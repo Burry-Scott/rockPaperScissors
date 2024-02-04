e0fc08d272a021f4d6fe44796bc919d909faa753
Barry Scott <burry.scott*@gmail.com>
2024-02-03 23:31:59 -0500

    add function that plays a single round of rock paper scissors

--- myscripts.js
+++ myscripts.js
@@ -11,8 +11,8 @@ let playerSelection = function getUserInput() {
               playerSelection != 'PAPER' &&
               playerSelection != 'SCISSORS');
 
-    switch (playerSelection) {
-        case 'ROCK':
+    switch (playerSelection) {                                      // For this game rock will be 1, paper will be 2,
+        case 'ROCK':                                                // and scissors will be 3. 
             playerSelection = 1;
             break;
         case 'PAPER':
@@ -26,5 +26,44 @@ let playerSelection = function getUserInput() {
     return playerSelection;
 }
 
+computerSelection();
 playerSelection();
 console.log(playerSelection);
+console.log(computerSelection);
+let resultOfRound = 0; 
+
+
+function playRound() {
+    let x = playerSelection;
+    let y = computerSelection; 
+    
+    if (x == 1 && y == 1) {                                          //Incrementing the result positive for player
+        return resultOfRound;                                         // and negative for the computer. 
+    } else if (x == 1 && y == 2) {        
+        resultOfRound = --resultOfRound;
+    } else if (x == 1 && y == 3) {
+        resultOfRound = ++resultOfRound;
+        return resultOfRound;
+    } else if (x == 2 && y == 1) {
+        resultOfRound = ++resultOfRound;
+        return resultOfRound;
+    } else if (x == 2 && y == 2) {
+        return resultOfRound;
+    } else if (x == 2 && y == 3) {
+        resultOfRound = --resultOfRound;
+        return resultOfRound;
+    } else if (x == 3 && y == 1) {
+        resultOfRound = --resultOfRound;
+        return resultOfRound;
+    } else if (x == 3 && y == 2) {
+        resultOfRound = ++resultOfRound;
+        return resultOfRound;
+    } else {
+        return resultOfRound;
+    }
+}
+
+
+
+playRound();
+console.log(resultOfRound);
