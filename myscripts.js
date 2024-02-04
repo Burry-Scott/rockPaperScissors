5c1445c4e45970827002548bea0478ac22015718
Barry Scott <burry.scott*@gmail.com>
2024-02-04 00:30:47 -0500

    add function that plays a game of 5 rounds

--- myscripts.js
+++ myscripts.js
@@ -1,41 +1,43 @@
-let computerSelection = function getComputerChoice() {         
-    computerSelection = Math.floor(Math.random() * (3-0) + 1);      
-    return computerSelection;
+let computerChoice;
+let resultOfRound; 
+let resultOfGame;
+let playerChoice; 
+
+function getComputerChoice() { 
+    computeChoice = 0;        
+    computerChoice = Math.floor(Math.random() * (3-0) + 1);      
+    return computerChoice;
 }
 
-let playerSelection = function getUserInput() {
+function getPlayerChoice() {
     do {
-        playerSelection = prompt("Please select Paper, Rock, or Scissors");
-        playerSelection = playerSelection.toUpperCase();
-    }   while(playerSelection != 'ROCK' &&
-              playerSelection != 'PAPER' &&
-              playerSelection != 'SCISSORS');
-
-    switch (playerSelection) {                                      // For this game rock will be 1, paper will be 2,
+        playerChoice = prompt("Please select Paper, Rock, or Scissors");
+        playerChoice = playerChoice.toUpperCase();
+    }   while(playerChoice != 'ROCK' &&
+              playerChoice != 'PAPER' &&
+              playerChoice != 'SCISSORS');
+    
+    switch (playerChoice) {                                         // For this game rock will be 1, paper will be 2,
         case 'ROCK':                                                // and scissors will be 3. 
-            playerSelection = 1;
+            playerChoice = 1;
             break;
         case 'PAPER':
-            playerSelection = 2;
+            playerChoice = 2;
             break;
         case 'SCISSORS':
-            playerSelection = 3;
+            playerChoice = 3;
             break;
     }
-    
-    return playerSelection;
+    return playerChoice;
 }
-
-computerSelection();
-playerSelection();
-console.log(playerSelection);
-console.log(computerSelection);
-let resultOfRound = 0; 
 
 
 function playRound() {
-    let x = playerSelection;
-    let y = computerSelection; 
+    resultOfRound = 0;
+    getComputerChoice();
+    getPlayerChoice();
+    let x = playerChoice;
+    let y = computerChoice; 
     
     if (x == 1 && y == 1) {                                          //Incrementing the result positive for player
         return resultOfRound;                                         // and negative for the computer. 
@@ -63,7 +65,30 @@ function playRound() {
     }
 }
 
+function playgame() {
+    resultOfGame = 0
+    for ( let i = 0; i < 5; i++) {                                     //Runs 5 rounds for each game
+        playRound();
+        if (resultOfRound == 1) {
+            resultOfGame++;
+            console.log("You won that one.");
+        } else if (resultOfRound == -1) {
+            resultOfGame--;
+            console.log("You lost that one.");
+        } else {
+            resultOfGame = resultOfGame;
+            console.log("That one was a tie.");
+        }
+    }
+
+    if (resultOfGame > 0) {
+        console.log("You WIN!");
+    } else if (resultOfGame < 0) {
+        console.log("You LOSE!");
+    } else {
+        console.log("Its a tie!!");
+    }
+}
 
 
-playRound();
-console.log(resultOfRound);
+playgame();
